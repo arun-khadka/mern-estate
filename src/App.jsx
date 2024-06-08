8c59187971b1f4e2842973d9efc13bdb2c690f97
arunKhadka <arunkhadka009@gmail.com>
2024-06-08 16:48:11 +0545

    initialized packages for development

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-xl text-red-500 font-normal">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
