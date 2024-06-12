5a2c563092b15fe2aa241bf366da959fec49db23
arunKhadka <arunkhadka009@gmail.com>
2024-06-12 16:32:15 +0545

    created middleware to handle errors

--- api/index.js
+++ api/index.js
@@ -12,6 +12,16 @@ dotenv.config();
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 const port = process.env.PORT;
 
 mongoose
